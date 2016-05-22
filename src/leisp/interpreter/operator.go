541c652448a551819e2780897e3471e1db751341
Zongmin Lei <leizongmin@gmail.com>
2016-05-23 00:30:03 +0800

    fix operator

--- src/leisp/interpreter/operator.go
+++ src/leisp/interpreter/operator.go
@@ -20,17 +20,13 @@ func CallFunction(s *types.Scope, args []*types.Atom) *types.Atom {
 	if l < 1 {
 		return types.NewErrorMessageAtom("invalid s-expression")
 	}
-	op := args[0]
-	switch op.Value.GetType() {
-
-	case "symbol":
-		return callFunction(s, op, args[1:])
-
-	case "keyword":
-		return types.NewErrorMessageAtom("keyword s-expression is not implementation")
-
+	switch args[0].Value.(type) {
+	case *types.SymbolValue:
+		return callFunction(s, args[0], args[1:])
+	case *types.KeywordValue:
+		return types.NewErrorMessageAtom("does not implement keyword s-expression")
 	default:
-		return types.NewErrorAtom(fmt.Errorf("invalid s-expression, operator cannot be type %s", op))
+		return types.NewErrorAtom(fmt.Errorf("invalid s-expression, operator cannot be type %s", args[0].Value.GetType()))
 	}
 }
 
