8bee8aaffaf0fb3fa45786dc7c5f034cc3401092
Zongmin Lei <leizongmin@gmail.com>
2016-05-22 23:56:15 +0800

    Fix Atom.ToString()

--- src/leisp/types/atom.go
+++ src/leisp/types/atom.go
@@ -38,7 +38,7 @@ func (a *Atom) ToString() string {
 	if a.Error != nil {
 		return fmt.Sprintf("<Error#\"%s\">", a.Error)
 	}
-	return fmt.Sprint(a.Value)
+	return fmt.Sprint(a.Value.ToString())
 }
 
 func (a *Atom) Print() {
